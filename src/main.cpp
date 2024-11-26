c65a664de5c1a2b056092a2efe51d2bc56bb04ec
Evann Lim <evannl@uci.edu>
2024-11-26 15:43:47 -0800

    Setup LILYGO board

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,34 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <TFT_eSPI.h>
+
+#include <Servo.h>
+#include <chrono>
+#include <iostream>
+
+// Objects
+TFT_eSPI tft = TFT_eSPI();
+Servo myservo;
+
+// Number coordinates
+const int x_coordinate = 0;
+const int y_coordinate = 0;
+const int font_size = 50;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+
+  // Initialize LILYGO screen
+  tft.begin();
+  tft.setRotation(0);
+
+  // Set background and font
+  tft.setTextSize(font_size);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  tft.drawNumber(0, x_coordinate, y_coordinate);
+  delay(200);
 }
