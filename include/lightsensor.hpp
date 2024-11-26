321a3151e7dfce3a08511f0956721cd296bb2f9d
ringo23333 <92695626+ringo23333@users.noreply.github.com>
2024-11-26 15:45:55 -0800

    Basic lightsensor read + convert

---
+++ include/lightsensor.hpp
@@ -0,0 +1,11 @@
+// constant values
+const int callibrateDuration = 10;
+const int maxDegree = 179;
+const int minDegree = 0;
+const int maxLightSensorValue = 4095;
+const int minLightSensorValue = 0;
+
+// Function declarations
+int lightSensorTick(Servo servo, const int servoPin, const int lightSensorPin);
+int convertAnalogToDegrees(int analogValue);
+int convertAnalogToIntensity(int analogueValue);
