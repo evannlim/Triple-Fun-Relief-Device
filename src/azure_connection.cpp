7a791043f4c8320d32578c6ed654ebebaf526b5b
ringo23333 <92695626+ringo23333@users.noreply.github.com>
2024-12-13 16:31:57 -0800

    Created azure_connection.cpp

---
+++ src/azure_connection.cpp
@@ -0,0 +1,196 @@
+#include <Arduino.h>
+#include <HTTPClient.h>
+#include <Wifi.h>
+#include <inttypes.h>
+#include <stdio.h>
+
+
+#include "esp_system.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+
+
+char ssid[50];  // your network SSID (name)
+char pass[50];  // your network password (use for WPA, or use
+                // as key for WEP)
+
+const String iothubName = "147Group38";
+const String deviceName = "147esp32";
+
+const char * root_ca = \
+  "-----BEGIN CERTIFICATE-----\n" \
+  "MIIEtjCCA56gAwIBAgIQCv1eRG9c89YADp5Gwibf9jANBgkqhkiG9w0BAQsFADBh\n" \
+  "MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n" \
+  "d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBH\n" \
+  "MjAeFw0yMjA0MjgwMDAwMDBaFw0zMjA0MjcyMzU5NTlaMEcxCzAJBgNVBAYTAlVT\n" \
+  "MR4wHAYDVQQKExVNaWNyb3NvZnQgQ29ycG9yYXRpb24xGDAWBgNVBAMTD01TRlQg\n" \
+  "UlMyNTYgQ0EtMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMiJV34o\n" \
+  "eVNHI0mZGh1Rj9mdde3zSY7IhQNqAmRaTzOeRye8QsfhYFXSiMW25JddlcqaqGJ9\n" \
+  "GEMcJPWBIBIEdNVYl1bB5KQOl+3m68p59Pu7npC74lJRY8F+p8PLKZAJjSkDD9Ex\n" \
+  "mjHBlPcRrasgflPom3D0XB++nB1y+WLn+cB7DWLoj6qZSUDyWwnEDkkjfKee6ybx\n" \
+  "SAXq7oORPe9o2BKfgi7dTKlOd7eKhotw96yIgMx7yigE3Q3ARS8m+BOFZ/mx150g\n" \
+  "dKFfMcDNvSkCpxjVWnk//icrrmmEsn2xJbEuDCvtoSNvGIuCXxqhTM352HGfO2JK\n" \
+  "AF/Kjf5OrPn2QpECAwEAAaOCAYIwggF+MBIGA1UdEwEB/wQIMAYBAf8CAQAwHQYD\n" \
+  "VR0OBBYEFAyBfpQ5X8d3on8XFnk46DWWjn+UMB8GA1UdIwQYMBaAFE4iVCAYlebj\n" \
+  "buYP+vq5Eu0GF485MA4GA1UdDwEB/wQEAwIBhjAdBgNVHSUEFjAUBggrBgEFBQcD\n" \
+  "AQYIKwYBBQUHAwIwdgYIKwYBBQUHAQEEajBoMCQGCCsGAQUFBzABhhhodHRwOi8v\n" \
+  "b2NzcC5kaWdpY2VydC5jb20wQAYIKwYBBQUHMAKGNGh0dHA6Ly9jYWNlcnRzLmRp\n" \
+  "Z2ljZXJ0LmNvbS9EaWdpQ2VydEdsb2JhbFJvb3RHMi5jcnQwQgYDVR0fBDswOTA3\n" \
+  "oDWgM4YxaHR0cDovL2NybDMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0R2xvYmFsUm9v\n" \
+  "dEcyLmNybDA9BgNVHSAENjA0MAsGCWCGSAGG/WwCATAHBgVngQwBATAIBgZngQwB\n" \
+  "AgEwCAYGZ4EMAQICMAgGBmeBDAECAzANBgkqhkiG9w0BAQsFAAOCAQEAdYWmf+AB\n" \
+  "klEQShTbhGPQmH1c9BfnEgUFMJsNpzo9dvRj1Uek+L9WfI3kBQn97oUtf25BQsfc\n" \
+  "kIIvTlE3WhA2Cg2yWLTVjH0Ny03dGsqoFYIypnuAwhOWUPHAu++vaUMcPUTUpQCb\n" \
+  "eC1h4YW4CCSTYN37D2Q555wxnni0elPj9O0pymWS8gZnsfoKjvoYi/qDPZw1/TSR\n" \
+  "penOgI6XjmlmPLBrk4LIw7P7PPg4uXUpCzzeybvARG/NIIkFv1eRYIbDF+bIkZbJ\n" \
+  "QFdB9BjjlA4ukAg2YkOyCiB8eXTBi2APaceh3+uBLIgLk8ysy52g2U3gP7Q26Jlg\n" \
+  "q/xKzj3O9hFh/g==\n" \
+  "-----END CERTIFICATE-----\n";
+
+const String sasToken = "SharedAccessSignature sr=147Group38.azure-devices.net%2Fdevices%2F147esp32&sig=IV4EthQsp0nx4YTaacicQ7TnffKxzwkZ6ZXuz5JuwN4%3D&se=1732305567";
+
+void store_login_to_memory() {
+  Serial.begin(9600);
+  delay(1000);
+
+  // Initialize NVS
+  esp_err_t err = nvs_flash_init();
+  if (err == ESP_ERR_NVS_NO_FREE_PAGES ||
+      err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    // NVS partition was truncated and needs to be erased
+    // Retry nvs_flash_init
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    err = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(err);
+
+  Serial.printf("\n");
+  Serial.printf("Opening Non-Volatile Storage (NVS) handle... ");
+  nvs_handle_t my_handle;
+  err = nvs_open("storage", NVS_READWRITE, &my_handle);
+  if (err != ESP_OK) {
+    Serial.printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
+  } else {
+    Serial.printf("Done\n");
+
+    // Write
+    Serial.printf("Updating ssid/pass in NVS ... ");
+    char ssid[] = "YOUR SSID";
+    char pass[] = "YOUR PASSWORD";
+    err = nvs_set_str(my_handle, "ssid", ssid);
+    err |= nvs_set_str(my_handle, "pass", pass);
+    Serial.printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
+
+    // Commit written value.
+    // After setting any values, nvs_commit() must be called to ensure changes
+    // are written to flash storage. Implementations may write to storage at
+    // other times, but this is not guaranteed.
+    Serial.printf("Committing updates in NVS ... ");
+    err = nvs_commit(my_handle);
+    Serial.printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
+
+    // Close
+    nvs_close(my_handle);
+    
+  }
+}
+
+// Code from lab 4
+void nvs_access() {
+  // Initialize NVS
+  esp_err_t err = nvs_flash_init();
+  if (err == ESP_ERR_NVS_NO_FREE_PAGES ||
+      err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    // NVS partition was truncated and needs to be erased
+    // Retry nvs_flash_init
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    err = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(err);
+
+  // Open
+  Serial.printf("\n");
+  Serial.printf("Opening Non-Volatile Storage (NVS) handle... ");
+  nvs_handle_t my_handle;
+  err = nvs_open("storage", NVS_READWRITE, &my_handle);
+  if (err != ESP_OK) {
+    Serial.printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
+  } else {
+    Serial.printf("Done\n");
+    Serial.printf("Retrieving SSID/PASSWD\n");
+    size_t ssid_len;
+    size_t pass_len;
+    err = nvs_get_str(my_handle, "ssid", ssid, &ssid_len);
+    err |= nvs_get_str(my_handle, "pass", pass, &pass_len);
+    switch (err) {
+      case ESP_OK:
+        Serial.printf("Done\n");
+        //Serial.printf("SSID = %s\n", ssid);
+        //Serial.printf("PASSWD = %s\n", pass);
+        break;
+      case ESP_ERR_NVS_NOT_FOUND:
+        Serial.printf("The value is not initialized yet!\n");
+        break;
+      default:
+        Serial.printf("Error (%s) reading!\n", esp_err_to_name(err));
+    }
+  }
+
+  // Close
+  nvs_close(my_handle);
+}
+
+//Code from lab 4
+int connect_to_wifi() {
+  Serial.begin(9600);
+  delay(1000);
+  // Retrieve SSID/PASSWD from flash before anything else
+  nvs_access();
+
+  // We start by connecting to a WiFi network
+  delay(1000);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, pass);
+
+  int counter = 0;
+
+  while (WiFi.status() != WL_CONNECTED) {
+    if (counter > 60) {
+        Serial.print("Connection failed after 30 seconds");
+        return 1;
+    }
+    delay(500);
+    Serial.print(".");
+    counter++;
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("MAC address: ");
+  Serial.println(WiFi.macAddress());
+  return 0;
+}
+
+void send_data() {
+  HTTPClient http;
+  String url = "https://" + iothubName + ".azure-devices.net/devices/" +
+                deviceName + "/messages/events?api-version=2016-11-14";
+  http.begin(url, root_ca); //Specify the URL and certificate
+  http.addHeader("Authorization", sasToken);
+  http.addHeader("Content-Type", "application/json");
+  std::string payload = "1 2 3 4 5 6 7 8 9";
+  int httpCode = http.POST(payload.c_str());
+
+  Serial.println("HTTP POST sent");
+  Serial.println(httpCode);
+}
+
+
